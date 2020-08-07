2ed014bde4cc949e4596db70ed14e82f5abda3af
Chris Thompson <chris.tx86@gmail.com>
2020-08-06 22:20:49 -0700

    style form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,31 @@ function Contact() {
     return (
         <div className="contact">
             <div className="contact__left">
+                <form className="contact__form">
+                    <div className="form__top">
+                        <div className="form">
+                            <input type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">Name</span>
+                            </label>
+                        </div>
+                        <div className="form">
+                            <input type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">E-mail</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="form__bottom">
+                        <div className="form">
+                            <textarea type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">Message</span>
+                            </label>
+                        </div>
 
+                    </div>
+                </form>
             </div>
             <div className="contact__right">
                 <div className="contact__email">
