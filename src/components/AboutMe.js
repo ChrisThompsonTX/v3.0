5ba9da2eab321492a9e18e4802fe937571c2d442
Chris Thompson <chris.tx86@gmail.com>
2020-08-06 21:00:12 -0700

    style about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,11 +5,27 @@ function AboutMe() {
     return (
         <div className="aboutme">
             <div className="aboutme__left">
-                <div className="aboutme_frame">
-                    <img alt="aboutme__portrait" src="https://tappd-seeds.s3-us-west-1.amazonaws.com/v3/me.jpeg"/>
+                <div className="aboutme__frame">
+                    <div className="aboutme__frameinner">
+                        <img alt="aboutme__portrait" src="https://tappd-seeds.s3-us-west-1.amazonaws.com/v3/me.jpeg"/>
+                    </div>
                 </div>
             </div>
-            <div className="aboutme__info"></div>
+            <div className="aboutme__right">
+                <div className="aboutme__header">
+                    <div className="aboutme__name">
+                        <h1>chris</h1>
+                        <h2>Thompson</h2>
+                    </div>
+                    <h3>Software Engineer | San Francisco Bay Area</h3>
+                </div>
+                <div className="aboutme__body">
+
+                </div>
+                <div className="aboutme__footer">
+
+                </div>
+            </div>
         </div>
     )
 }
