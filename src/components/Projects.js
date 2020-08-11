7f54160700c95fa7b6e7c7fd765d234e5c2536d7
Chris Thompson <chris.tx86@gmail.com>
2020-08-10 21:18:11 -0700

    add favicon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.css'
 
 function Projects() {
+    const [open, setOpen] = useState(false);
+
     return (
         <div className="projects">
-            projects
+            <div className="projects__left">
+
+            </div>
+            <div className="projects__right">
+
+            </div>
         </div>
     )
 }
