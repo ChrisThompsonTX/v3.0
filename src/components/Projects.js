5b895ed169a3d62f25e3e0eb41cd6e63d67d4d6b
Chris Thompson <chris.tx86@gmail.com>
2020-08-10 23:12:07 -0700

    add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
                 <ProjectItem 
                     title="Chicken Tinder"
                     date="3/15 - 6/1"
-                    tech="(MongoDB, Javascript, Express, React, Node, Mapbox,  Yelp API)"
+                    tech="(MongoDB, Javascript, Express, React, Node, Mapbox, Yelp API)"
                     description="A single page app designed for groups to vote on a restaurant for their next gathering based on group location and filters."
                     onClick={() => setOpen("Chicken Tinder")}
                 />
@@ -26,13 +26,37 @@ function Projects() {
                 <ProjectItem 
                     title="Wiki-Clicker"
                     date="4/19 - 5/2"
-                    tech="(Javascript, HTML, CSS, D3, Regex)"
+                    tech="(Javascript, D3, Regex)"
                     description="A game utilizing Javascripts D3 library and Wikipedia API to navigate from one starter page to another in 10 link “clicks”"
                     onClick={() => setOpen("Wiki-Clicker")}
                 />
+                <ProjectItem 
+                    title="Instagram Clone"
+                    date="8/1 - 8/10"
+                    tech="(Javascript, React Hooks, Firebase)"
+                    description="Full Instagram clone with user auth and backend using Google Firebase"
+                    onClick={() => setOpen("Instagram Clone")}
+                />
+                <ProjectItem 
+                    title="Paralax-Scroll"
+                    date="6/29 - 7/1"
+                    tech="(Javascript, HTML, CSS)"
+                    description="A first attempt at building out a Paralax Scroll"
+                    onClick={() => setOpen("Paralax Scroll")}
+                />
+                <ProjectItem 
+                    title="React Expense Tracker"
+                    date="6/25 - 6/27"
+                    tech="(React, Hooks, Javascript)"
+                    description="React app to keep track of total expenses using React Hooks"
+                    onClick={() => setOpen("React Expense Tracker")}
+                />
             </div>
             <div className="projects__right">
-
+                {open === "Chicken Tinder" ?
+                    <iframe src="https://lit-atoll-81167.herokuapp.com/#/" height="100%" width="100%" frameBorder="0" ></iframe>
+                :null
+                }
             </div>
         </div>
     )
