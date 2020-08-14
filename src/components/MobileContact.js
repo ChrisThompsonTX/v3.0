6aac276a0dcc0bd8585caa24888858f4bab1b3e2
Chris Thompson <chris.tx86@gmail.com>
2020-08-14 14:18:19 -0700

    style mobile contact component

--- src/components/MobileContact.js
+++ src/components/MobileContact.js
@@ -5,7 +5,33 @@ function MobileContact({ open }) {
     return (
         <div className={open === "Contact" ? "mobilecontact open" : "mobilecontact"} >
             <div className={open === "Contact" ? "mobilecontact__child fade" : "mobilecontact__child"}>
-                Sup
+                <h1>Contact Me</h1>
+                <form className="contact__form">
+                    <div className="form__top">
+                        <div className="form">
+                            <input type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">Name</span>
+                            </label>
+                        </div>
+                        <div className="spacer" />
+                        <div className="form">
+                            <input type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">E-mail</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="form__bottom">
+                        <div className="form">
+                            <textarea type="text" name="name" autocomplete="off" required />
+                            <label for="name" className="label-name">
+                                <span className="content-name">Message</span>
+                            </label>
+                        </div>
+                    </div>
+                    <input type="submit" name="" value="Send" />
+                </form>
            </div>
         </div>
     )
