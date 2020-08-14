030b3da26604fa1e3ee4a3acbcf76a222aa5813e
Chris Thompson <chris.tx86@gmail.com>
2020-08-13 23:57:00 -0700

    style mobile about me

--- src/components/MobileAboutMe.js
+++ src/components/MobileAboutMe.js
@@ -13,7 +13,7 @@ function MobileAboutMe({ open }) {
                     <h1>Thompson</h1>
                 </div>
                 <div className="mobileaboutme__body">
-                    skadjflkasjdklfjaskfjlksadj fkljsalkdf klsajdflka jsdklfj aslkflkasd flka sfjlksd flkjalks fkjas flkaslf jljalkf 
+                    I am a Full Stack Software Developer with a passion for understanding the way things work and building and refactoring systems. For the last two years I have been using JavaScript, Node, React, Redux, jQuerry, Postgresql, CSS and HTML5 to build my own webapps and have recently sharpened my skills at App Academy, learning Ruby on Rails, and the MERN stack. I’m thrilled to be writing to apply for the Software Engineer  position at Frisbie Inc and apply my skill-set at a company that values being on the cutting edge of technology.
                 </div>
            </div>
         </div>
