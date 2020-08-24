9cbfb8816cb63e0ebb0d4ae550e7be41db030be1
Chris Thompson <chris.tx86@gmail.com>
2020-08-24 13:58:30 -0700

    edit view menu

--- src/components/ViewDropdownMenu.js
+++ src/components/ViewDropdownMenu.js
@@ -34,24 +34,7 @@ function ViewDropdownMenu(props) {
                 onEnter={calcHeight}
             >
                 <div className="menu">
-                    <DropdownItem onClick={() => props.setOpen("About Me")}>About Chris Thompson</DropdownItem>
-                    <DropdownItem
-                        leftIcon={<CogIcon />}
-                        rightIcon={<ChevronIcon />}
-                        goToMenu="settings"
-                        setActiveMenu={setActiveMenu}
-                    >
-                        Notable Skills
-                    </DropdownItem>
-                    <DropdownItem
-                        leftIcon={<i className="fas fa-folder-open"></i>}
-                        rightIcon={<ChevronIcon />}
-                        goToMenu="animals"
-                        setActiveMenu={setActiveMenu}
-                    >
-                        Recent Projects
-                        </DropdownItem>
-
+                    
                 </div>
             </CSSTransition>
 
@@ -62,42 +45,6 @@ function ViewDropdownMenu(props) {
                 unmountOnExit
                 onEnter={calcHeight}>
                 <div className="menu">
-                    <DropdownItem
-                        goToMenu="main"
-                        leftIcon={<ArrowIcon />}
-                        setActiveMenu={setActiveMenu}
-                    >
-                    </DropdownItem>
-                    <DropdownItem leftIcon={<i className="fab fa-js-square"></i>}>Javascript</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fab fa-react"></i>}>React/Redux</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fas fa-gem"></i>}>Ruby</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fab fa-python"></i>}>Python</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fas fa-database"></i>}>SQL/noSQL databases</DropdownItem>
-                    <DropdownItem leftIcon={<BoltIcon />}>jQuery</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fab fa-node"></i>}>Node</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fas fa-database"></i>}>MongoDB</DropdownItem>
-                    <DropdownItem leftIcon={<BoltIcon />}>Express</DropdownItem>
-                    <DropdownItem leftIcon={<i className="fab fa-aws"></i>}>AWS</DropdownItem>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === 'animals'}
-                timeout={500}
-                classNames="menu-secondary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem
-                        goToMenu="main"
-                        leftIcon={<ArrowIcon />}
-                        setActiveMenu={setActiveMenu}
-                    >
-                    </DropdownItem>
-                    <DropdownItem leftIcon="🐔">Chicken Tinder</DropdownItem>
-                    <DropdownItem leftIcon="🍻">Tappd</DropdownItem>
-                    <DropdownItem leftIcon="🔍">Wiki-Clicker</DropdownItem>
-                    <DropdownItem leftIcon="🎨">Amity-Lianne</DropdownItem>
                 </div>
             </CSSTransition>
         </div>
