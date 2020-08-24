164e3c33a76f2a35e591c897476966639007d8f8
Chris Thompson <chris.tx86@gmail.com>
2020-08-24 15:35:23 -0700

    style left projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,55 +21,61 @@ class Projects extends React.Component {
         return (
             <div className="projects">
                 <div className="projects__left">
-                    <ProjectItem 
-                        title="Chicken Tinder"
-                        date="3/15 - 6/1"
-                        tech="(MongoDB, Javascript, Express, React, Node, Mapbox, Yelp API)"
-                        description="A single page app designed for groups to vote on a restaurant for their next gathering based on group location and filters."
-                        setOpen={this.setOpen}
-                    />
-                    <ProjectItem 
-                        title="Tappd"
-                        date="2/1 - 5/10"
-                        tech="(Ruby on Rails, React, Redux, AWS)"
-                        description="A social media platform centered around beer utilizing a Ruby on Rails backend and React and Redux frontend."
-                        setOpen={this.setOpen}
-                    />
-                    <ProjectItem 
-                        title="Wiki-Clicker"
-                        date="4/19 - 5/2"
-                        tech="(Javascript, D3, Regex)"
-                        description="A game utilizing Javascripts D3 library and Wikipedia API to navigate from one starter page to another in 10 link “clicks”"
-                        setOpen={() => this.setOpen("Wiki-Clicker")}
-                    />
-                    <ProjectItem 
-                        title="Instagram Clone"
-                        date="8/1 - 8/10"
-                        tech="(Javascript, React Hooks, Firebase)"
-                        description="Full Instagram clone with user auth and backend using Google Firebase"
-                        setOpen={() => this.setOpen("Instagram Clone")}
-                    />
-                    <ProjectItem 
-                        title="Paralax-Scroll"
-                        date="6/29 - 7/1"
-                        tech="(Javascript, HTML, CSS)"
-                        description="A first attempt at building out a Paralax Scroll"
-                        setOpen={() => this.setOpen("Paralax-Scroll")}
-                    />
-                    <ProjectItem 
-                        title="React Expense Tracker"
-                        date="6/25 - 6/27"
-                        tech="(React, Hooks, Javascript)"
-                        description="React app to keep track of total expenses using React Hooks"
-                        setOpen={() => this.setOpen("React Expense Tracker")}
-                    />
-                    <ProjectItem 
-                        title="Amity Lianne"
-                        date="6/29 - 7/10"
-                        tech="(Javascript, HTML, CSS)"
-                        description="A mobile friendly portfolio site made for my moms art for her Birthday"
-                        setOpen={() => this.setOpen("Amity Lianne")}
-                    />
+                    <div className="projects__leftheader" >
+                        <h4>Recent Projects</h4>
+                        <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div className="projects__leftbody">
+                        <ProjectItem 
+                            title="Chicken Tinder"
+                            date="3/15 - 6/1"
+                            tech="(MongoDB, Javascript, Express, React, Node, Mapbox, Yelp API)"
+                            description="A single page app designed for groups to vote on a restaurant for their next gathering based on group location and filters."
+                            setOpen={this.setOpen}
+                        />
+                        <ProjectItem 
+                            title="Tappd"
+                            date="2/1 - 5/10"
+                            tech="(Ruby on Rails, React, Redux, AWS)"
+                            description="A social media platform centered around beer utilizing a Ruby on Rails backend and React and Redux frontend."
+                            setOpen={this.setOpen}
+                        />
+                        <ProjectItem 
+                            title="Wiki-Clicker"
+                            date="4/19 - 5/2"
+                            tech="(Javascript, D3, Regex)"
+                            description="A game utilizing Javascripts D3 library and Wikipedia API to navigate from one starter page to another in 10 link “clicks”"
+                            setOpen={() => this.setOpen("Wiki-Clicker")}
+                        />
+                        <ProjectItem 
+                            title="Instagram Clone"
+                            date="8/1 - 8/10"
+                            tech="(Javascript, React Hooks, Firebase)"
+                            description="Full Instagram clone with user auth and backend using Google Firebase"
+                            setOpen={() => this.setOpen("Instagram Clone")}
+                        />
+                        <ProjectItem 
+                            title="Paralax-Scroll"
+                            date="6/29 - 7/1"
+                            tech="(Javascript, HTML, CSS)"
+                            description="A first attempt at building out a Paralax Scroll"
+                            setOpen={() => this.setOpen("Paralax-Scroll")}
+                        />
+                        <ProjectItem 
+                            title="React Expense Tracker"
+                            date="6/25 - 6/27"
+                            tech="(React, Hooks, Javascript)"
+                            description="React app to keep track of total expenses using React Hooks"
+                            setOpen={() => this.setOpen("React Expense Tracker")}
+                        />
+                        <ProjectItem 
+                            title="Amity Lianne"
+                            date="6/29 - 7/10"
+                            tech="(Javascript, HTML, CSS)"
+                            description="A mobile friendly portfolio site made for my moms art for her Birthday"
+                            setOpen={() => this.setOpen("Amity Lianne")}
+                        />
+                    </div>
                 </div>
                 <div className="projects__right">
                     {this.state.open === "Chicken Tinder" ? 
