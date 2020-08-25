fd2bac95f2ed73be3f4c72848bd714ba18ab52e9
Chris Thompson <chris.tx86@gmail.com>
2020-08-24 21:16:17 -0700

    update navbar links to open in new tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ function Navbar(props) {
         <nav className="navbar">
             <ul className="navbar-nav">{props.children}</ul>
             <div className="navbar__links">
-                <a href="https://www.linkedin.com/in/chris-thompson-832015179/">
+                <a href="https://www.linkedin.com/in/chris-thompson-832015179/" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-linkedin"></i></a>
-                <a href="https://github.com/ChrisThompsonTX">
+                <a href="https://github.com/ChrisThompsonTX" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-github"></i></a>
-                <a href="https://angel.co/u/christopher-thio-thompson">
+                <a href="https://angel.co/u/christopher-thio-thompson" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-angellist"></i></a>
                 <i className="fas fa-battery-full"></i>
                 <Clock />
